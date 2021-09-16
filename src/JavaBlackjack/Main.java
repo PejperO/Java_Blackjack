e19f1ece78cd228aa1c0c8c79ddbc45c41516428
Sumii <64231313+PejperO@users.noreply.github.com>
2021-09-16 15:59:09 +0200

    PejperO

--- src/JavaBlackjack/Main.java
+++ src/JavaBlackjack/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("board.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("BlackJack");
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
