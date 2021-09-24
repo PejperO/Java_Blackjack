0882c31691a9894a0c8b7c3770680e5cd80ea7e5
Sumii <64231313+PejperO@users.noreply.github.com>
2021-09-24 20:46:04 +0200

    PejperO

--- Java_Blackjack/src/BJ_Window.java
+++ Java_Blackjack/src/BJ_Window.java
@@ -3,11 +3,11 @@ import java.awt.*;
 
 public class BJ_Window extends JFrame {
 
-    public static int windowSizeX = 1_280;
+    public static int windowSizeX = 1_280;  //window size...
     public static int windowSizeY = 920;
 
     public BJ_Window() {
-        super("Black Jack");
+        super("Black Jack");    //making window look better
         setSize(windowSizeX, windowSizeY);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ImageIcon imageICO = new ImageIcon("data/img/icon.png");
@@ -16,80 +16,59 @@ public class BJ_Window extends JFrame {
 
         buttonPanel();
 
-        BJ_Screen bj = new BJ_Screen();
+        BJ_Screen bj = new BJ_Screen(); //for now just background
         add(bj);
     }
 
     public static void main(String[] args) {
-        BJ_Window bj = new BJ_Window();
+        BJ_Window bj = new BJ_Window(); //making window
         bj.setVisible(true);
     }
 
     public void buttonPanel(){
-        JButton buttonHit = new JButton();
+
+        JButton buttonHit = new JButton();  //setting button
         buttonHit.setBounds(880, 750, 90, 100);
-        ImageIcon iconHit = new ImageIcon("data/img/hit.png");
-        buttonHit.setHorizontalTextPosition(JButton.CENTER);
-        buttonHit.setIcon(iconHit);
+        buttonHit.setIcon(new ImageIcon("data/img/hit.png"));
         buttonHit.setText("HIT");
-        buttonHit.setFont(new Font("Comic Sans", Font.BOLD,20));
-        buttonHit.setVerticalTextPosition(JButton.BOTTOM);
-        buttonHit.setForeground(Color.BLACK);
-        buttonHit.setFocusable(false);
-        buttonHit.setVisible(true);
-        buttonHit.setOpaque(false);
-        buttonHit.setContentAreaFilled(false);
-        buttonHit.setBorderPainted(false);
+        buttonProperties(buttonHit);    //more properties
+        buttonHit.addActionListener(e -> System.out.println("HIT :>")); //after click - shot message
         add(buttonHit);
 
         JButton buttonDouble = new JButton();
         buttonDouble.setBounds(700, 750, 130, 100);
-        ImageIcon iconDouble = new ImageIcon("data/img/double.png");
-        buttonDouble.setHorizontalTextPosition(JButton.CENTER);
-        buttonDouble.setIcon(iconDouble);
+        buttonDouble.setIcon(new ImageIcon("data/img/double.png"));
         buttonDouble.setText("DOUBLE");
-        buttonDouble.setFont(new Font("Comic Sans", Font.BOLD,20));
-        buttonDouble.setVerticalTextPosition(JButton.BOTTOM);
-        buttonDouble.setForeground(Color.BLACK);
-        buttonDouble.setFocusable(false);
-        buttonDouble.setVisible(true);
-        buttonDouble.setOpaque(false);
-        buttonDouble.setContentAreaFilled(false);
-        buttonDouble.setBorderPainted(false);
+        buttonProperties(buttonDouble);
+        buttonDouble.addActionListener(e -> System.out.println("DOUBLE :>"));
         add(buttonDouble);
 
         JButton buttonSplit = new JButton();
         buttonSplit.setBounds(460, 750, 90, 100);
-        ImageIcon iconSplit = new ImageIcon("data/img/split.png");
-        buttonSplit.setHorizontalTextPosition(JButton.CENTER);
-        buttonSplit.setIcon(iconSplit);
+        buttonSplit.setIcon(new ImageIcon("data/img/split.png"));
         buttonSplit.setText("SPLIT");
-        buttonSplit.setFont(new Font("Comic Sans", Font.BOLD,20));
-        buttonSplit.setVerticalTextPosition(JButton.BOTTOM);
-        buttonSplit.setForeground(Color.BLACK);
-        buttonSplit.setFocusable(false);
-        buttonSplit.setVisible(true);
-        buttonSplit.setOpaque(false);
-        buttonSplit.setContentAreaFilled(false);
-        buttonSplit.setBorderPainted(false);
-        //buttonSplit.setBorder(BorderFactory.createEtchedBorder());
+        buttonProperties(buttonSplit);
+        buttonSplit.addActionListener(e -> System.out.println("SPLIT :>"));
         add(buttonSplit);
 
-        JButton buttonStay = new JButton();
+        JButton buttonStay = new JButton(); // I know there is a vay to compress the buttons
         buttonStay.setBounds(320, 750, 90, 100);
-        ImageIcon iconStay = new ImageIcon("data/img/stay.png");
-        buttonStay.setHorizontalTextPosition(JButton.CENTER);
-        buttonStay.setIcon(iconStay);
+        buttonStay.setIcon(new ImageIcon("data/img/stay.png"));
         buttonStay.setText("STAY");
-        buttonStay.setFont(new Font("Comic Sans", Font.BOLD,20));
-        buttonStay.setVerticalTextPosition(JButton.BOTTOM);
-        buttonStay.setForeground(Color.BLACK);
-        buttonStay.setFocusable(false);
-        buttonStay.setVisible(true);
-        buttonStay.setOpaque(false);
-        buttonStay.setContentAreaFilled(false);
-        buttonStay.setBorderPainted(false);
+        buttonProperties(buttonStay);
+        buttonStay.addActionListener(e -> System.out.println("STAY :>"));
         add(buttonStay);
-        //buttonStay.setEnabled(false);
+    }
+
+    private void buttonProperties(JButton button) { //mutual button properties
+        button.setFont(new Font("Comic Sans", Font.BOLD,20));    //font
+        button.setHorizontalTextPosition(JButton.CENTER);   //making button look better
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setForeground(Color.BLACK);
+        button.setFocusable(false);
+        button.setVisible(true);
+        button.setOpaque(false);
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
     }
 }
